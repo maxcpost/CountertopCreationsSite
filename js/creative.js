96ef8d7f8e0a3d13b4890ad585a24d4f8df52691
Unknown <maxpost4811@gmail.com>
2018-08-03 01:42:11 -0400

    client changes
    Header "fix", image carousel instead of the grid layout for the #portfolio section

--- js/creative.js
+++ js/creative.js
@@ -15,6 +15,10 @@
     }
   });
 
+  $('.carousel').carousel({
+    interval: 2000
+  })
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
